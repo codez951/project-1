04b9a4de12d500542546b82d4f7834e1cf67ef8e
cute Z <aryansatyam.mainacc@gmail.com>
2026-05-29 04:20:38 +0530

    Working Main Scene

--- mob.gd
+++ mob.gd
@@ -6,11 +6,12 @@ func _ready() -> void:
 	var mob_types = Array($AnimatedSprite2D.sprite_frames.get_animation_names())
 	$AnimatedSprite2D.animation = mob_types.pick_random()
 	$AnimatedSprite2D.play()
-	$VisibleOnScreenNotifier2D.screen_exited.connect(_on_visible_on_screen_notifier_2d_screen_exited())
-
-func _on_visible_on_screen_notifier_2d_screen_exited():
-	queue_free()
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
--- player.gd
+++ player.gd
@@ -5,9 +5,11 @@ signal hit
 @export var speed = 400
 var screen_size
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -40,11 +42,13 @@ func _process(delta: float) -> void:
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
 	
+
 func _on_body_entered(body: Node2D) -> void:
 	hide()
 	hit.emit()
 	$CollisionShape2D.set_deferred("disabled", true)
 	
+
 func start(pos):
 	position = pos
 	show()
