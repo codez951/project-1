2fcbbabdc768be8ee804d7fc80f73dcea9a13143
cute Z <aryansatyam.mainacc@gmail.com>
2026-05-29 05:00:28 +0530

    Very basic v1
    - Enemies don't despawn after game over.
    - Score Timer never stops after game over.

--- main.gd
+++ main.gd
@@ -7,15 +7,18 @@ func _ready():
 	pass
 
 
-func game_over() -> void:
+func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 	
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 
 func _on_mob_timer_timeout() -> void:
@@ -37,6 +40,7 @@ func _on_mob_timer_timeout() -> void:
 
 func _on_score_timer_timeout() -> void:
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_start_timer_timeout() -> void:
